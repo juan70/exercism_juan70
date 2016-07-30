f5aaa8e5375ea5fe3b87ce70e6a5191c6567b5d4
juan70 <juan70@users.noreply.github.com>
2016-07-30 20:35:02 +0000

    Java exercise strain modified to avoid using filter()...

--- java/strain/src/main/java/Strain.java
+++ java/strain/src/main/java/Strain.java
@@ -5,10 +5,18 @@ import java.util.function.Predicate;
 
 public class Strain
 {
+  /*
+   * I didn't read that we should not use the filter() function/method.
+   * I modify my program accordingly...
+   */
   public static <T> List<T> keep(List<T> list, Predicate<T> pred) {
-    return list.stream()
-               .filter(pred)
-               .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+    List<T> result = new ArrayList<>();
+
+    for (T elem : list) {
+      if (pred.test(elem)) { result.add(elem); }
+    }
+
+    return result;
   }
 
 
